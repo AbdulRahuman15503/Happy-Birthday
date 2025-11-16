9d7cd5dd0c60628d85402acd4b4ffb163802ba01
AbdulRahuman15503 <96279820+AbdulRahuman15503@users.noreply.github.com>
2025-11-16 11:48:37 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -75,7 +75,7 @@
         this.tree = tree;
 
         var scale = scale || 1
-        var color = '#FFC0CB';
+        var color = '#EECB9C';
 
         this.heart = {
             point  : point,
@@ -531,6 +531,7 @@
     window.Tree = Tree;
 
 })(window);
+
 
 
 
